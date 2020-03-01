f093c2bd2883705561fb66472a710b8d91026492
Tavita Su'a <tavitas@sprep.org>
2020-03-02 10:02:49 +1400

    override site description

--- src/pages/index.js
+++ src/pages/index.js
@@ -57,7 +57,7 @@ function Home() {
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Documentation for the SPREP Inform project and its deployed tools.">
       <header className={classnames('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
