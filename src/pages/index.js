8b8b62db6fe3be0827702c01a530553a202a4ef0
Tavita Su'a <tavitas@sprep.org>
2020-08-13 11:49:01 +1300

    added anchor links to homepage icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,48 +1,41 @@
-import React from 'react';
-import classnames from 'classnames';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import styles from './styles.module.css';
+import React from "react";
+import classnames from "classnames";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import styles from "./styles.module.css";
 
 const features = [
   {
     title: <>SPREP Inform</>,
-    imageUrl: 'img/inform.svg',
-    description: (
-      <>
-        General information on the SPREP Inform project.
-      </>
-    ),
+    imageUrl: "img/inform.svg",
+    description: <>General information on the SPREP Inform project.</>,
+    redirectURL: "docs/inform/introduction",
   },
   {
     title: <>Data Portals</>,
-    imageUrl: 'img/undraw_docusaurus_tree.svg',
-    description: (
-      <>
-        Help material on the deployed DKAN data data portals.
-      </>
-    ),
+    imageUrl: "img/undraw_docusaurus_tree.svg",
+    description: <>Help material on the deployed DKAN data data portals.</>,
+    redirectURL: "docs/dataportal/introduction",
   },
   {
     title: <>Indicator Reporting Tool</>,
-    imageUrl: 'img/undraw_docusaurus_react.svg',
-    description: (
-      <>
-        Help material for the Indicator Reporting Tool.
-      </>
-    ),
+    imageUrl: "img/undraw_docusaurus_react.svg",
+    description: <>Help material for the Indicator Reporting Tool.</>,
+    redirectURL: "docs/irt/intro",
   },
 ];
 
-function Feature({imageUrl, title, description}) {
+function Feature({ imageUrl, title, description, redirectURL }) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
-    <div className={classnames('col col--4', styles.feature)}>
+    <div className={classnames("col col--4", styles.feature)}>
       {imgUrl && (
-        <div className="text--center">
-          <img className={styles.featureImage} src={imgUrl} alt={title} />
+        <div className='text--center'>
+          <a href={useBaseUrl(redirectURL)}>
+            <img className={styles.featureImage} src={imgUrl} alt={title} />
+          </a>
         </div>
       )}
       <h3>{title}</h3>
@@ -53,22 +46,24 @@ function Feature({imageUrl, title, description}) {
 
 function Home() {
   const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
+  const { siteConfig = {} } = context;
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="Documentation for the SPREP Inform project and its deployed tools.">
-      <header className={classnames('hero hero--primary', styles.heroBanner)}>
-        <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
+      description='Documentation for the SPREP Inform project and its deployed tools.'
+    >
+      <header className={classnames("hero hero--primary", styles.heroBanner)}>
+        <div className='container'>
+          <h1 className='hero__title'>{siteConfig.title}</h1>
+          <p className='hero__subtitle'>{siteConfig.tagline}</p>
           <div className={styles.buttons}>
             <Link
               className={classnames(
-                'button button--outline button--secondary button--lg',
-                styles.getStarted,
+                "button button--outline button--secondary button--lg",
+                styles.getStarted
               )}
-              to={useBaseUrl('docs/inform/introduction')}>
+              to={useBaseUrl("docs/inform/introduction")}
+            >
               Get Started
             </Link>
           </div>
@@ -77,8 +72,8 @@ function Home() {
       <main>
         {features && features.length && (
           <section className={styles.features}>
-            <div className="container">
-              <div className="row">
+            <div className='container'>
+              <div className='row'>
                 {features.map((props, idx) => (
                   <Feature key={idx} {...props} />
                 ))}
