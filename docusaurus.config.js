fa4dbd8166e32738ced962e8a3c10b5292e9df5b
Tavita Su'a <tavitas@sprep.org>
2022-01-31 11:54:29 +1300

    updating search apikey

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,7 +42,8 @@ module.exports = {
     algolia: {
       appId: "USH05RCS6I",
       // apiKey: "48fd31306dc294463d102f37c7bb580f",
-      apiKey: "f9f23a2100faa168222413fbeb4bc41d",
+      apiKey: "ac51b715a1b415a0626c65fd0f8a0c3d",
+      // apiKey: "f9f23a2100faa168222413fbeb4bc41d",
       // apiKey: "d21c15fa49cb0cafe787c200328c1904",
       indexName: "sprep_inform",
       // algoliaOptions: {}, // Optional, if provided by Algolia
